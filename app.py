ce15a08d0a571df2e9057548245576a582596116
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-01-29 21:27:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import json
 import requests
-from livereload import Server
+import pandas as pd
+import random
 
-app = Flask(__name__)
-
-#@app.route('/')
-
-#def welcome():
- #   return "Hello"
+app = Flask(_name_)
 
 @app.route('/', methods=['GET','POST'])
-
 def facts():
-    import numpy as np
-    import pandas as pd
-    import random
-    import time
     data = pd.read_csv('facts.csv')
-    df=pd.DataFrame(data , columns = ['Facts'])
-    while True:
-        dff=df.values.tolist()
-        value=random.choices(dff)
-        #print(value)
-        time.sleep(10)
-        return render_template('facts.html',value=value)
-        #return " {} ".format(value)
-       # return " {} ".format(value)
-        #return [(" {} ".format(value)) , render_template('refresh.html')]
-        #return " {} ".format(facts())
-        #def refresh():
-    #return render_template('refresh.html')
-    #return " {} ".format(value)
-#@app.route('/', methods=['GET','POST'])
-#def refresh():
- #   return facts
-#@app.route('/facts', methods=['GET','POST'])
-#def refresh():
-    #return render_template('refresh.html')
-#def index():
- #   return 'Hello'
- #   #return render_template('index.html')
+    facts = list(data['Facts'])
+    value=(random.choices(facts))
+    #print(type(value[0]))
+    data = {
+       'fact' : value[0],
+    }
+    data = json.dumps(data)
+    return jsonify(data)
 
-
-
-if __name__=='__main__':
-    server = Server(app.wsgi_app)
+if _name=='main_':
+    #server = Server(app.wsgi_app)
     #server.serve()
-    server.watch('/Views/*')
-    app.run(debug=False)
-    app.config['DEBUG'] = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    #server.watch('/Views/*')
+    app.run(debug=True)
+    #app.config['DEBUG'] = True
+    #app.config['TEMPLATES_AUTO_RELOAD'] = True
